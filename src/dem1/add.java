bb7b5ce6c036afcab00a7101571d0b508af45711
raghuv868g <raghuv868g@gmail.com>
2019-07-07 00:22:44 +0530

    name added

--- src/dem1/add.java
+++ src/dem1/add.java
@@ -7,9 +7,15 @@ public class add {
 		System.out.println(c);
 		return c;
 	}
+	public String name(String name) {
+		System.out.println(name);
+		return name;
+	}
 	public static void main(String[] args) {
 		add l=new add();
 		int k=l.add(12, 34);	
+		System.out.println("the value is "+k);
+		String g=l.name("raghu");
 	}
 
 }
